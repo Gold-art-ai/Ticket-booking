5f416a2a0f778a0b94c1e857336eb47d3e4a92d2
Ishara Gold <isharagold6@gmail.com>
2026-08-10 00:52:02 +0200

    test: declare mocks and first service test skeleton

--- bookingTickets/src/test/java/com/example/bookingTickets/service/TicketServiceTest.java
+++ bookingTickets/src/test/java/com/example/bookingTickets/service/TicketServiceTest.java
@@ -1,9 +1,23 @@
 package com.example.bookingTickets.service;
 
+import com.example.bookingTickets.model.Ticket;
+import com.example.bookingTickets.repository.TicketRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
 class TicketServiceTest {
+
+    @Mock
+    private TicketRepository repository;
+
+    @InjectMocks
+    private TicketService ticketService;
+
+    @Test
+    void createTicket_shouldReturnSavedTicket() {
+    }
 }
