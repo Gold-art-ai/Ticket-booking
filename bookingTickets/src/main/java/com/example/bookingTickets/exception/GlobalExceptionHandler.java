49a06046ad8d2e0024c5e433131915dbb40d2502
Ishara Gold <isharagold6@gmail.com>
2026-08-10 00:36:25 +0200

    feat: handle TicketNotFoundException with 404 response

--- bookingTickets/src/main/java/com/example/bookingTickets/exception/GlobalExceptionHandler.java
+++ bookingTickets/src/main/java/com/example/bookingTickets/exception/GlobalExceptionHandler.java
@@ -1,7 +1,14 @@
 package com.example.bookingTickets.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+    @ExceptionHandler(TicketNotFoundException.class)
+    public ResponseEntity<String> handleTicketNotFoundException(TicketNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
 }
